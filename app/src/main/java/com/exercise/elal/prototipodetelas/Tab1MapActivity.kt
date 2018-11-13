70ad55b5d4955363b848b51a5c516873b4d7b7b7
Elal <elymaralves@gmail.com>
2018-11-13 17:24:50 -0200

    ajustes da aba "mapa"

--- app/src/main/java/com/exercise/elal/prototipodetelas/Tab1MapActivity.kt
+++ app/src/main/java/com/exercise/elal/prototipodetelas/Tab1MapActivity.kt
@@ -5,11 +5,52 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_tab_map.*
 
-class Tab1MapActivity : Fragment(){
+class Tab1MapActivity : Fragment(), OnMapReadyCallback{
+
+    lateinit var gMap: GoogleMap
+    lateinit var mapV: MapView
+    lateinit var mView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_tab_map,container, false)
+        mView = inflater.inflate(R.layout.fragment_tab_map,container, false)
+        return mView
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapV = mView.findViewById(R.id.mapView)
+        if(mapV != null) {
+            mapV.onCreate(null)
+            mapV.onResume()
+            mapV.getMapAsync(this)
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        MapsInitializer.initialize(context)
+        val ceagri = LatLng(-8.017724, -34.944367)
+
+        gMap = googleMap
+        gMap.addMarker(MarkerOptions().position(ceagri).title("CREAGRI II").snippet("João Vasconcelos Sobrinho"))
+
+        val jvs : CameraPosition = CameraPosition.builder().target(ceagri).zoom(16F).bearing(0F).build()
+
+        gMap.moveCamera(CameraUpdateFactory.newCameraPosition(jvs))
+
+
+    }
+
 
 }
